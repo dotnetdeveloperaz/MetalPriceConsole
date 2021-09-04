aa102c20bcba143075baaee81a6ebabc956e935e
Scott Glasgow <dotnetdeveloperaz@gmail.com>
2021-09-04 17:02:15 +0300

    Added unique key to avoid duplicate entries

--- SQL/CreateGold.sql
+++ SQL/CreateGold.sql
@@ -1,5 +1,4 @@
 -- Personal.GoldRates definition
-
 CREATE TABLE `GoldPrices` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Price` double NOT NULL,
@@ -8,5 +7,6 @@ CREATE TABLE `GoldPrices` (
   `Chg` double NOT NULL,
   `ChgPct` double NOT NULL,
   `AddDate` timestamp NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=289 DEFAULT CHARSET=utf8mb4;
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `GoldPrices_UN` (`RateDate`)
+) ENGINE=InnoDB AUTO_INCREMENT=590 DEFAULT CHARSET=utf8mb4;
