08fd942b514a91d0e9c87ac62535d3564094e704
Scott Glasgow <=>
2023-04-23 00:33:50 -0700

    Updated code for version 2.1 changes. #22 #23 #24

--- Commands/Status.cs
+++ Commands/Status.cs
@@ -1,4 +1,10 @@
+using System;
 using System.ComponentModel;
+using System.Threading;
+using GoldPriceConsole.Models;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using RestSharp;
 using Spectre.Console;
 using Spectre.Console.Cli;
 
@@ -6,6 +12,15 @@ namespace GoldPriceConsole.Commands;
 
 public class StatusCommand : Command<StatusCommand.Settings>
 {
+    private readonly ApiServer _apiServer;
+    private ILogger _logger;
+
+    public StatusCommand(ApiServer apiServer, ILogger<AccountCommand> logger)
+    {
+        _apiServer = apiServer;
+        _logger = logger;
+    }
+
     public class Settings : CommandSettings
     {
         [Description("Get WebApi Status.")]
@@ -25,6 +40,65 @@ public class StatusCommand : Command<StatusCommand.Settings>
     public override int Execute(CommandContext context, Settings settings)
     {
         settings.Status = true;
+        if (settings.Debug)
+        {
+            DebugDisplay.Print(settings, _apiServer, _logger);
+        }
+        // Process Window
+        var table = new Table().Centered();
+        table.HideHeaders();
+        table.BorderColor(Color.Yellow);
+        table.Border(TableBorder.Rounded);
+        table.Border(TableBorder.Simple);
+        table.AddColumns(new[] { "", "" });
+        table.Expand();
+
+        // Animate
+        AnsiConsole
+            .Live(table)
+            .AutoClear(false)
+            .Overflow(VerticalOverflow.Ellipsis)
+            .Cropping(VerticalOverflowCropping.Top)
+            .Start(ctx =>
+            {
+                void Update(int delay, Action action)
+                {
+                    action();
+                    ctx.Refresh();
+                    Thread.Sleep(delay);
+                }
+                Update(
+                    70,
+                    () =>
+                        table.Columns[0].Footer(
+                            $"[red bold]Status[/] [green bold]Retrieving WebApi Status[/]"
+                        )
+                );
+                // Content
+                var client = new RestClient(_apiServer.BaseUrl + "status");
+                var request = new RestRequest("", Method.Get);
+                request.AddHeader("x-access-token", _apiServer.Token);
+                request.AddHeader("Content-Type", "application/json");
+                RestResponse response;
+                ApiStatus apiStatus;
+                try
+                {
+                    response = client.Execute(request);
+                    apiStatus = JsonConvert.DeserializeObject<ApiStatus>(response.Content);
+                }
+                catch (Exception ex)
+                {
+                    Update(70, () => table.AddRow($"[red]Error: {ex.Message}[/]", $"[red]Calling Url: {_apiServer.BaseUrl}status[/]"));
+                    return;
+                }
+                table.Columns[1].RightAligned().Width(30).PadRight(20);
+                table.Columns[0].RightAligned();
+                if(apiStatus.result)
+                    Update(70, () => table.AddRow($"[yellow]WebApi Status[/]", "[green]Service is Up[/]"));
+                else
+                    Update(70, () => table.AddRow($"[yellow]WebApi Status[/]", "[red]Service is Down[/]"));
+                Update(70, () => table.Columns[0].Footer("[blue]Complete[/]"));
+            });
         return 0;
     }
 }
--- Database.cs
+++ Database.cs
@@ -11,19 +11,29 @@ namespace GoldPriceConsole
 {
     internal class Database
     {
-        private List<GoldPrice> _goldPrices;
-
+        /// <summary>
+        /// Saves collection of GoldPrice basically looping through the 
+        /// collection and called the single GoldPrice method.
+        /// </summary>
+        /// <param name="goldPrices"></param>
+        /// <param name="connectionString"></param>
+        /// <returns></returns>
         public static bool Save(List<GoldPrice> goldPrices, string connectionString)
         {
             int success = 0;
-            foreach(GoldPrice goldPrice in goldPrices)
+            foreach (GoldPrice goldPrice in goldPrices)
             {
                 if (Save(goldPrice, connectionString))
                     success++;
             }
             return (success == goldPrices.Count);
         }
-
+        /// <summary>
+        /// Saves gold price for specific day.
+        /// </summary>
+        /// <param name="goldPrice">Type GoldPrice</param>
+        /// <param name="connectionString"Database connectionstring></param>
+        /// <returns></returns>
         public static bool Save(GoldPrice goldPrice, string connectionString)
         {
             if (goldPrice.date.Year < 1900)
@@ -45,8 +55,16 @@ namespace GoldPriceConsole
             {
                 Console.WriteLine("Could not insert new gold rate.");
                 Console.WriteLine("Exception: {0}", ex.Message);
-                string result = JsonConvert.SerializeObject(goldPrice, Formatting.Indented);
-                File.AppendAllText($"GoldPrice.cache", result);
+                List<GoldPrice> goldPrices = new List<GoldPrice>();
+                if (File.Exists("GoldPrice.cache"))
+                {
+                    var file = File.ReadAllText("GoldPrice.cache");
+                    goldPrices = JsonConvert.DeserializeObject<List<GoldPrice>>(file);
+                }
+                goldPrices.Add(goldPrice);
+                string result = JsonConvert.SerializeObject(goldPrices, Formatting.Indented);
+                File.WriteAllText($"GoldPrice.cache", result);
+
                 return false;
             }
             finally
