0dae61b3641f4714967d07210a6f8785ba17c710
Scott Glasgow <dotnetdeveloperaz@gmail.com>
2023-07-30 11:57:17 +0300

    Removed unnecessary using

--- Commands/History.cs
+++ Commands/History.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.ComponentModel.Design;
 using System.IO;
 using System.Threading;
 using MetalPriceConsole.Models;
