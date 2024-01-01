43a29c6cb027b83702337b6040242f7457e55874
dotnetdeveloperaz <dotnetdeveloperaz@gmail.com>
2024-01-01 07:59:44 -0700

    Added fake support

--- Commands/Account.cs
+++ Commands/Account.cs
@@ -1,5 +1,4 @@
 using System;
-using System.ComponentModel;
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading;
@@ -7,13 +6,16 @@ using System.Threading.Tasks;
 using Spectre.Console;
 using Spectre.Console.Cli;
 using MetalPriceConsole.Models;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 
 namespace MetalPriceConsole.Commands;
 
 public class AccountCommand : AsyncCommand<AccountCommand.Settings>
 {
     private readonly ApiServer _apiServer;
-    private static readonly string[] columns = new[] { "", ""};
+    private static readonly string[] columns = new[] { "" };
 
     public AccountCommand(ApiServer apiServer)
     {
@@ -22,25 +24,23 @@ public class AccountCommand : AsyncCommand<AccountCommand.Settings>
 
     public class Settings : BaseCommandSettings
     {
-        [Description("Get Account Statistics.")]
-        [DefaultValue(false)]
-        public bool Account { get; set; }
+        // There are no special settings for this command
     }
 
     public override async Task<int> ExecuteAsync(CommandContext context, Settings settings)
     {
-        settings.Account = true;
         string url = _apiServer.BaseUrl + "stat";
         if (settings.Debug)
         {
             DebugDisplay.Print(settings, _apiServer, url);
         }
-        AnsiConsole.WriteLine();
+        //AnsiConsole.WriteLine();
+        //AnsiConsole.WriteLine();
         // Process Window
         var table = new Table().Centered();
-        table.BorderColor(Color.Yellow);
+        table.HideHeaders();
+        table.BorderColor(Color.Green3);
         table.Border(TableBorder.Rounded);
-        table.Border(TableBorder.Simple);
         table.AddColumns(columns);
         table.Expand();
 
@@ -58,8 +58,7 @@ public class AccountCommand : AsyncCommand<AccountCommand.Settings>
                     ctx.Refresh();
                     Thread.Sleep(delay);
                 }
-                // Borders
-                // Footers
+
                 Update(
                     70,
                     () =>
@@ -68,45 +67,40 @@ public class AccountCommand : AsyncCommand<AccountCommand.Settings>
                         )
                 );
                 // Content
+                Update(70, () => table.Columns[0].Footer($"[green]Calling {url}[/]"));
                 Account account;
-                HttpClient client = new();
-                client.DefaultRequestHeaders.Add("x-access-token", _apiServer.Token);
-                using (HttpRequestMessage request = new(HttpMethod.Get, url))
+                try
                 {
-                    try
+                    if(!settings.Fake)
+                        account = await AccountDetails.GetDetailsAsync(_apiServer, url);
+                    else
                     {
-                        Update(70, () => table.Columns[0].Footer($"[green]Calling {request.RequestUri.AbsoluteUri}[/]"));
-                        HttpResponseMessage response = await client.SendAsync(request);
-                        response.EnsureSuccessStatusCode();
-                        var result = await response.Content.ReadAsStreamAsync();
-                        account = await JsonSerializer.DeserializeAsync<Account>(result);
-                    }
-                    catch (HttpRequestException ex)
-                    {
-                        Update(70, () => table.AddRow($"[red]Request Error: {ex.Message}[/]", $"[red]Calling Url: {_apiServer.BaseUrl}stat[/]"));
-                        return;
-                    }
-                    catch (Exception ex)
-                    {
-                        Update(70, () => table.AddRow($"[red]Error: {ex.Message}[/]", $"[red]Calling Url: {_apiServer.BaseUrl}stat[/]"));
-                        return;
+                        string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                        string file = Path.Combine(path, "Account.sample");
+                        string cache = File.ReadAllText(file);
+                        account = JsonSerializer.Deserialize<Account>(cache);
+
                     }
                 }
-                table.Columns[1].RightAligned().Width(30).PadRight(20);
-                table.Columns[0].RightAligned();
-                Update(70, () => table.AddRow($"[yellow]Requests Today[/]", $"[yellow]{account.RequestsToday}[/]"));
-                Update(70, () => table.AddRow($"[yellow]Requests Yesterday[/]", $"[yellow]{account.RequestsYesterday}[/]"));
-                Update(70, () => table.AddRow($"[yellow]Requests This Month[/]", $"[yellow]{account.RequestsMonth}[/]"));
-                Update(70, () => table.AddRow($"[yellow]Requests Last Month[/]", $"[yellow]{account.RequestsLastMonth}[/]"));
+                catch ( Exception ex ) 
+                {
+                    Update(70, () => table.AddRow($"[red]Request Error: {ex.Message} Calling Url: {_apiServer.BaseUrl}stat[/]"));
+                    return;
+                }
+
+                table.Columns[0].Centered();
+                Update(70, () => table.AddRow($"[yellow]     Requests Today[/] [yellow]{account.RequestsToday}[/]"));
+                Update(70, () => table.AddRow($"[yellow] Requests Yesterday[/] [yellow]{account.RequestsYesterday}[/]"));
+                Update(70, () => table.AddRow($"[yellow]Requests This Month[/] [yellow]{account.RequestsMonth}[/]"));
+                Update(70, () => table.AddRow($"[yellow]Requests Last Month[/] [yellow]{account.RequestsLastMonth}[/]"));
 
                 _ = int.TryParse(_apiServer.MonthlyAllowance, out int allowance);
                 Update(
                     70,
                     () =>
                     table.AddRow(
-                        $":check_mark: [green bold italic]Remaining WebAPI Requests:[/]", 
-                        $"[green bold italic]{allowance - account.RequestsMonth}[/]"));
-                Update(70, () => table.Columns[1].Footer("[blue]Complete[/]"));
+                        $":check_mark: [green bold italic]Remaining WebAPI Requests: {allowance - account.RequestsMonth}[/]"));
+                Update(70, () => table.Columns[0].Footer($"[blue]Finished Retrieving Account Details From {url}[/]"));
             });
         return 0;
     }
