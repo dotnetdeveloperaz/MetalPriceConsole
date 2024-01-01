496074ec9d6ab618347119dd581b7a460a299d28
dotnetdeveloperaz <dotnetdeveloperaz@gmail.com>
2024-01-01 08:04:07 -0700

    Code cleanup

--- Commands/BaseCommandSettings.cs
+++ Commands/BaseCommandSettings.cs
@@ -1,10 +1,5 @@
-﻿using Spectre.Console.Cli;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using Spectre.Console.Cli;
 
 namespace MetalPriceConsole.Commands
 {
