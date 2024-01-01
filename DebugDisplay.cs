72c22280b50a9a30dcdcfd8c7c307db2dfcb655f
dotnetdeveloperaz <dotnetdeveloperaz@gmail.com>
2024-01-01 10:38:56 -0700

    Updated the debug display to be more friendly

--- DebugDisplay.cs
+++ DebugDisplay.cs
@@ -9,11 +9,11 @@ namespace MetalPriceConsole;
 
 public class DebugDisplay
 {
-    internal static void Print(CommandSettings settings, ApiServer server, string Url)
+    internal static bool Print(CommandSettings settings, ApiServer server, string Url)
     {
-        Print(settings, server, "", Url);
+        return Print(settings, server, "", Url);
     }
-    internal static void Print(CommandSettings settings, ApiServer server, string connectionString, string Url)
+    internal static bool Print(CommandSettings settings, ApiServer server, string connectionString, string Url)
     {
         // Debug Window
         var table = new Table().Centered();
@@ -21,60 +21,51 @@ public class DebugDisplay
         table.Border(TableBorder.DoubleEdge);
         table.Expand();
 
-        // Animate
-        AnsiConsole
-            .Live(table)
-            .AutoClear(false)
-            .Overflow(VerticalOverflow.Ellipsis)
-            .Cropping(VerticalOverflowCropping.Top)
-            .Start(ctx =>
+        // Columns
+        table.AddColumn("Setting Key");
+        table.AddColumn("Value");
+
+        // Column alignment
+        table.Columns[0].RightAligned();
+        table.Columns[1].RightAligned();
+
+        bool isDebug = false;
+        bool showHidden = false;
+        PropertyInfo[] properties = settings.GetType().GetProperties();
+        foreach (PropertyInfo property in properties)
+        {
+            object value = property.GetValue(settings);
+            if (property.Name == "Debug")
+                isDebug = (bool)value;
+            if (property.Name == "ShowHidden")
+                showHidden = (bool)value;
+        }
+        if (isDebug)
+        {
+            if (showHidden)
             {
-                void Update(int delay, Action action)
-                {
-                    action();
-                    ctx.Refresh();
-                    Thread.Sleep(delay);
-                }
+                if (connectionString != String.Empty)
+                    table.AddRow($"Database Connection:", $"{connectionString}");
+                table.AddRow($"Token:", $"{server.Token}");
+                table.AddRow($"Url:", $"{Url}");
+            }
+            foreach (PropertyInfo property in properties)
+            {
+                object value = property.GetValue(settings);
 
-                // Columns
-                Update(70, () => table.AddColumn("Setting Key"));
-                Update(70, () => table.AddColumn("Value"));
+                if (property.PropertyType == typeof(bool))
+                    table.AddRow($"{property.Name}", $"[yellow]{(bool)value}[/]");
+                else
+                    if (value is not null)
+                    table.AddRow($"{property.Name}", $"{value}");
+            }
+            AnsiConsole.Write(table);
+            if (AnsiConsole.Confirm("Continue?"))
+                Title.Print();
+            else
+                return false;
 
-                // Column alignment
-                Update(70, () => table.Columns[0].RightAligned());
-                Update(70, () => table.Columns[1].RightAligned());
-
-                bool isDebug = false;
-                bool showHidden = false;
-                PropertyInfo[] properties = settings.GetType().GetProperties();
-                foreach (PropertyInfo property in properties)
-                {
-                    object value = property.GetValue(settings);
-                    if (property.Name == "Debug")
-                        isDebug = (bool)value;
-                    if (property.Name == "ShowHidden")
-                        showHidden = (bool)value;
-                }
-                if (isDebug)
-                {
-                    if (showHidden)
-                    {
-                        if (connectionString != String.Empty)
-                            Update(70, () => table.AddRow($"Database Connection:", $"{connectionString}"));
-                        Update(70, () => table.AddRow($"Token:", $"{server.Token}"));
-                        Update(70, () => table.AddRow($"Url:", $"{Url}"));
-                    }
-                    foreach (PropertyInfo property in properties)
-                    {
-                        object value = property.GetValue(settings);
- 
-                        if (property.PropertyType == typeof(bool))
-                            Update(70, () => table.AddRow($"{property.Name}", $"[yellow]{(bool)value}[/]"));
-                        else
-                            if (value is not null)
-                                Update(70, () => table.AddRow($"{property.Name}", $"{value}"));
-                    }
-                }
-            });
+        }
+        return true;
     }
 }
